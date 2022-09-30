7d3bba0771d90d1563b4ee4c92d7f74a765b461b
salsabilafnp <salsabilafebriantynp@gmail.com>
2022-09-30 15:24:23 +0700

    ubah halaman admin

--- pw/php/admin.php
+++ pw/php/admin.php
@@ -1,18 +1,13 @@
 <?php
 session_start();
 
-if (!isset($_SESSION["username"])) {
-  header("location: login.php");
-  exit;
-}
-
 require 'functions.php';
 
-$shoes = query("SELECT * FROM cheval");
+$books = query("SELECT * FROM buku");
 
 // ketika tombol cari ditekan
 if (isset($_POST['cari'])) {
-  $shoes = cari($_POST['keyword']);
+  $books = cari($_POST['keyword']);
 }
 
 ?>
@@ -25,10 +20,10 @@ if (isset($_POST['cari'])) {
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- CSS -->
-  <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
-  <link rel="stylesheet" href="../assets/css/style.css">
-  <link rel="stylesheet" href="../assets/icons/font/bootstrap-icons.css">
-  <title>Cheval's - Admin Page</title>
+  <link rel="stylesheet" href="../../assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../../assets/css/style.css">
+  <link rel="stylesheet" href="../../assets/icons/font/bootstrap-icons.css">
+  <title>Pesona Store - Admin Page</title>
 </head>
 
 <body>
@@ -36,20 +31,7 @@ if (isset($_POST['cari'])) {
   <!-- Navbar -->
   <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-main mx-auto">
     <div class="container">
-      <a href="../php/admin.php" class="fw-bold fst-italic align-middle navbar-brand">CHEVAL</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-        <ul class="navbar-nav">
-          <a href="../shoe.php" class="nav-item nav-link page-scroll">
-            <button type="button" class="btn btn-2"><i class="bi bi bi-people-fill"></i> User Page</button>
-          </a>
-          <a href="../php/logout.php" class="nav-item nav-link page-scroll">
-            <button type="button" class="btn btn-1"><i class="bi bi-box-arrow-right"></i> Logout</button>
-          </a>
-        </ul>
-      </div>
+      <a href="../php/admin.php" class="fw-bold fst-italic align-middle navbar-brand">Pesona Store</a>
     </div>
   </nav>
 
@@ -70,17 +52,17 @@ if (isset($_POST['cari'])) {
     <table class="table table-sm align-middle table-bordered table-striped table-hover text-center">
       <thead>
         <th scope="col">No.</th>
-        <th scope="col">Opsi</th>
-        <th scope="col">Picture</th>
-        <th scope="col">Name</th>
-        <th scope="col">Available Color</th>
-        <th scope="col">Stocks</th>
-        <th scope="col">Price</th>
-        <th scope="col">Category</th>
+        <th scope="col">Aksi</th>
+        <th scope="col">Gambar</th>
+        <th scope="col">Judul Buku</th>
+        <th scope="col">Genre</th>
+        <th scope="col">Pengarang</th>
+        <th scope="col">Penerbit</th>
+        <th scope="col">Tahun Terbit</th>
       </thead>
 
       <!-- Tampilan Pencarian -->
-      <?php if (empty($shoes)) : ?>
+      <?php if (empty($books)) : ?>
         <tr>
           <td colspan="7">
             <h1 style="color: red;">Data tidak ditemukan</h1>
@@ -89,27 +71,23 @@ if (isset($_POST['cari'])) {
       <?php endif; ?>
 
       <?php $no = 1; ?>
-      <?php foreach ($shoes as $shoe) : ?>
+      <?php foreach ($books as $book) : ?>
         <tbody>
           <td><?= $no; ?></td>
           <td class="text-center col-1">
             <!-- button edit -->
-            <a href="ubah.php?id=<?= $shoe['id']; ?>" class="btn btn-sm btn-outline-primary m-1"><i class="bi bi-pencil-square d-md-block d-sm-none"></i> Edit</a>
+            <a href="ubah.php?id=<?= $book['id']; ?>" class="btn btn-sm btn-outline-primary m-1"><i class="bi bi-pencil-square d-md-block d-sm-none"></i> Edit</a>
             <!-- button delete -->
-            <a href="hapus.php?id=<?= $shoe['id']; ?>" class="delete btn btn-sm btn-outline-danger" onclick="return confirm('Delete This?')"><i class="bi bi-trash d-md-block d-sm-none"></i> Delete</a>
+            <a href="hapus.php?id=<?= $book['id']; ?>" class="delete btn btn-sm btn-outline-danger" onclick="return confirm('Delete This?')"><i class="bi bi-trash d-md-block d-sm-none"></i> Delete</a>
           </td>
           <td class="col-3">
-            <img class="m-1" src="../assets/img/<?= $shoe["picture"]; ?>" width="50%">
+            <img class="m-1" src="../../img/<?= $book['gambar']; ?>" width="50px">
           </td>
-          <td class="fw-bold text-uppercase col-2"><?= $shoe["name"]; ?></td>
-          <td class="col-2"><?= $shoe["color"]; ?></td>
-          <?php if ($shoe['stok'] > 0) : ?>
-            <td class="fw-bold"><?= $shoe["stok"]; ?></td>
-          <?php elseif ($shoe['stok'] < 1) : ?>
-            <td class="col-1 text-danger fw-bold">OUT OF STOCK</td>
-          <?php endif; ?>
-          <td class="fst-italic"><?= $shoe["price"]; ?></td>
-          <td class="fw-bold text-secondary"><?= $shoe["category"]; ?></td>
+          <td class="fw-bold text-uppercase col-2"><?= $book['judul_buku']; ?></td>
+          <td class="col-2"><?= $book['genre']; ?></td>
+          <td class="fst-italic"><?= $book['pengarang']; ?></td>
+          <td class="fst-italic"><?= $book['penerbit']; ?></td>
+          <td class="fw-bold text-secondary"><?= $book['tahun_terbit']; ?></td>
         </tbody>
         <?php $no++; ?>
       <?php endforeach; ?>
