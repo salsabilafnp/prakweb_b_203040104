6d871f0f2c4ff4d1ba7b392d8eb9ee8dbe23d987
salsabilafnp <salsabilafebriantynp@gmail.com>
2022-10-14 14:46:19 +0700

    mvc - video 3

--- mvc/app/core/App.php
+++ mvc/app/core/App.php
@@ -4,6 +4,18 @@ class App
 {
   public function __construct()
   {
-    echo 'OK!';
+    $url = $this->parseURL();
+    var_dump($url);
+  }
+
+  public function parseURL()
+  {
+    // ambil url dan dikembalikan
+    if (isset($_GET['url'])) {
+      $url = rtrim($_GET['url'], '/');
+      $url = filter_var($url, FILTER_SANITIZE_URL);
+      $url = explode('/', $url);
+      return $url;
+    }
   }
 }
